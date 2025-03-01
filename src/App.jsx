1e93e1773a7195cd8195707f70c7f38791b08c71
Cleve-keem <belloharyourmidey@gmail.com>
2025-03-01 03:09:12 -0800

    developing tap component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+      <ul className="tabs">
+        <li>Tab 1</li>
+        <li>Tab 2</li>
+        <li>Tab 3</li>
+        <li>Tab 4</li>
+      </ul>
+      <div className="tab-content">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
